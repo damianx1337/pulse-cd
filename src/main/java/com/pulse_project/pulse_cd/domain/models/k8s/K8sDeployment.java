c23768037595bf154027b1355527d0d27f8727f8
Dennis Sobczak <dn.sobc@gmail.com>
2025-11-23 12:58:50 +0100

    changed k8s deployment dto

--- src/main/java/com/pulse_project/pulse_cd/domain/models/k8s/K8sDeployment.java
+++ src/main/java/com/pulse_project/pulse_cd/domain/models/k8s/K8sDeployment.java
@@ -1,9 +1,10 @@
 package com.pulse_project.pulse_cd.domain.models.k8s;
 
-public record K8sDeployment(Integer id, String name, String imageName){
-    public K8sDeployment(Integer id, String name, String imageName) {
+public record K8sDeployment(Integer id, String name, String namespace, String imageName){
+    public K8sDeployment(Integer id, String name, String namespace, String imageName) {
         this.id = id;
         this.name = name;
+        this.namespace = namespace;
         this.imageName = imageName;
     }
 }
